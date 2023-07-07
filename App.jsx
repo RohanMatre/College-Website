1bdee4c45937d0cc612f2324c9cf2e7488177fb2
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-07-07 11:05:44 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Middle from "./Middle";
+// import Department from "./Department";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Middle />
+      {/* <Department /> */}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
